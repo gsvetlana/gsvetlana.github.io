bd22e34337237efd0f1aa6ed188d9f0ae3175ca8
Sveta Galytska <s-galytska@ukr.net>
2017-06-21 01:20:28 +0300

    Added menu to HW JS 9-10

--- js/galytska_JS_9-10/js/script.js
+++ js/galytska_JS_9-10/js/script.js
@@ -1,53 +1,36 @@
-$(function() {
+// $(function() {
+//     'use strict';
+//
+//     $('.sub-menu').hide();
+//     $('.dropdown').click(function(){
+//         $(this).next().css({
+//             'opacity':'0', 'display':'block'
+//         }).animate({opacity:'1'}, 700, 'easeInCirc');
+//     });
+//
+// });
+(function(){
     'use strict';
-    //carousel
-    $('.jcarousel').jcarousel({
-    });
 
-    $('.jcarousel-control-next')
-        .on('jcarouselcontrol:active', function() {
-            $(this).removeClass('inactive');
-        })
-        .on('jcarouselcontrol:inactive', function() {
-            $(this).addClass('inactive');
-        })
-        .jcarouselControl({
-            target: '+=1'
-        });
-
-    $('.jcarousel-control-prev')
-        .on('jcarouselcontrol:active', function() {
-            $(this).removeClass('inactive');
-        })
-        .on('jcarouselcontrol:inactive', function() {
-            $(this).addClass('inactive');
-        })
-        .jcarouselControl({
-            target: '-=1'
-        });
-
-    //select
-    $('select').wSelect();
-
-    //checkbox jQuery
-    $('.niceCheck').mousedown(function() {
-        changeCheck($(this));
-    });
-
-    function changeCheck(el) {
-        var $el = el,
-            $input = $el.find('input').eq(0);
-        if(!$input.attr('checked')) {
-            $input.attr('checked', true);
-
-            $el.css('background-position','-17px 0');
-        } else {
-            $input.attr('checked', false);
-
-            $el.css('background-position','0 0');
-        }
-        return true;
+    var submenu = document.getElementsByClassName('sub-menu'),
+        menu = document.getElementsByClassName('menu')[0];
+    console.log(submenu);
+    console.log(menu);
+    for(var i = 0, max = submenu.length; i < max; i++) {
+        submenu[i].style.display = 'none';
     }
-});
+
+    menu.addEventListener('click', function(event) {
+        var target = event.target;
+        if(!target.classList.contains('dropdown') &&
+            !target.classList.contains('fa-caret-down') &&
+            !target.classList.contains('fa-caret-right')) {
+            return;
+        }
+        target.closest('.dropdown').nextElementSibling.style.display = 'block';
+    });
+
+
+}());
 
 

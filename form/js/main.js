0acd5a7301438f2da44f0dd9b55a69c02a661aad
Sveta Galytska <s-galytska@ukr.net>
2017-07-10 23:13:43 +0300

    added script to write the user info down the table

--- form/js/main.js
+++ form/js/main.js
@@ -2,13 +2,13 @@
     'use strict';
 
     var wrapper = document.querySelector('.wrapper'),
-        arrData = new Array();
+        arrData = [];
 
     function getData() {
-        var name = document.getElementById('name'),
-            surname = document.getElementById('surname'),
-            tel = document.getElementById('tel'),
-            email = document.getElementById('email'),
+        var name = wrapper.querySelector('#name'),
+            surname = wrapper.querySelector('#surname'),
+            tel = wrapper.querySelector('#tel'),
+            email = wrapper.querySelector('#email'),
             newObj = {};
 
         newObj.name = name.value;
@@ -19,16 +19,46 @@
         arrData.push(newObj);
     }
 
-    var btn = document.querySelector('.addData');
-    btn.addEventListener('click', function(e) {
+    function createElements() {
+        var btn = document.createElement('button');
+        btn.classList.add('addData');
+        btn.innerHTML = 'Add data';
+
+        var table = document.createElement('table');
+        table.createCaption().innerHTML = 'User Info';
+        table.createTHead().innerHTML = '<th>Name</th><th>Surname</th><th>Phone number</th><th>Email address</th>';
+        table.appendChild(document.createElement('tbody'));
+
+        return {
+            btnItem: btn,
+            tableItem: table
+        };
+    }
+
+    var elem = createElements();
+    var getUserData = wrapper.querySelector('.get-data');
+    getUserData.appendChild(elem.btnItem);
+
+    elem.btnItem.addEventListener('click', function(e) {
         e.preventDefault();
+        elem.tableItem.tBodies[0].innerHTML = '';
+
         getData();
 
-        var sourceUser = document.querySelector('#userRow').textContent,
-            templateUser = _.template(sourceUser),
-            parent = document.querySelector('tbody');
+        wrapper.appendChild(elem.tableItem);
+        for(var i = 0, max = arrData.length; i < max; i++) {
+            var newRow = elem.tableItem.tBodies[0].insertRow(i);
+            for(var item in arrData[i]) {
+                if(arrData[i].hasOwnProperty(item)) {
+                    newRow.insertCell().innerHTML = arrData[i][item];
+                }
+            }
+        }
 
-            parent.innerHTML = templateUser({list: arrData});
+        var inputs = wrapper.querySelectorAll('input');
+        for(var k = 0, maxK = inputs.length; k < maxK; k++) {
+            inputs[k].value = '';
+        }
     });
 })();
 
